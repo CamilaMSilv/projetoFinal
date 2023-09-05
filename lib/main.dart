6dcd8f944c74165a54bd147d020b9beaa02c7cb9
Camila Martins <camiladeveloper@hotmail.com>
2023-09-04 21:52:55 -0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,40 +13,191 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
+  String _storedNumber = "0.00";
+  void _updateStoredNumber(String value) {
+    setState(() {
+      if (value == "C") {
+        _storedNumber = "0.00";
+      } else if (value == "Apagar") {
+        if (_storedNumber.isNotEmpty) {
+          _storedNumber = _storedNumber.substring(0, _storedNumber.length - 1);
+        }
+      } else {
+        if (_storedNumber == "0.00") {
+          _storedNumber = value;
+        } else {
+          _storedNumber += value;
+        }
+      }
+    });
+  }
+
+  Widget _buildNumberButton(String label) {
+    return ElevatedButton(
+      onPressed: () {
+        _updateStoredNumber(label);
+      },
+      child: Text(
+        label,
+        style: TextStyle(fontSize: 24),
+      ),
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.all(20),
+        minimumSize: Size(80, 80),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "MaterialApp",
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          brightness: Brightness.light, // Define o tema como dark
-          primaryColor: Colors.blue, //cor primaria do APP
-          fontFamily: 'Roboto', // Fonte padrão do aplicativo
-          appBarTheme: const AppBarTheme(color: Colors.deepPurple),
-        ),
-        home: Scaffold(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
           appBar: AppBar(
-            title: const Text("AppBar"),
-            actions: const [Icon(Icons.exit_to_app), Icon(Icons.edit_note)],
+            title: const Text("Avaliação 1"),
+            actions: <Widget>[
+              IconButton(
+                icon: const Icon(
+                  Icons.update,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  // do something
+                },
+              )
+            ],
           ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-            Container(width: 100, height: 50, color: Colors.yellow, margin: const EdgeInsets.all(5)),
-            Container(width: 200, height: 50, color: Colors.red, margin: const EdgeInsets.all(5)),
-            Container(width: 300, height: 50, color: Colors.green, margin: const EdgeInsets.all(5)),
-            Container(width: 400, height: 50, color: Colors.blue, margin: const EdgeInsets.all(5)),
-          ]),
-          drawer: const Drawer(),
-          bottomNavigationBar: BottomNavigationBar(items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Perfil"),
-          ]),
-          floatingActionButton: const FloatingActionButton(
-            onPressed: null,
-            child: Icon(Icons.add),
-          ),
-        ));
+          body: Column(children: <Widget>[
+            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              const Center(
+                heightFactor: 2,
+                child: Text('Valor',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold)),
+              ),
+              Container(
+                  padding: const EdgeInsets.only(
+                    left: 30,
+                    top: 5,
+                    right: 30,
+                    bottom: 0,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      TextField(
+                        keyboardType: const TextInputType.numberWithOptions(
+                            decimal: true),
+                        inputFormatters: <TextInputFormatter>[
+                          FilteringTextInputFormatter.allow(
+                              RegExp(r'[0-9]+[,.]{0,1}[0-9]*')),
+                          TextInputFormatter.withFunction(
+                            (oldValue, newValue) => newValue.copyWith(
+                              text: newValue.text.replaceAll('.', ','),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  )),
+            ]),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Container(
+                  padding: const EdgeInsets.only(
+                    left: 60,
+                    top: 15,
+                    right: 30,
+                    bottom: 0,
+                  ),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue, // background
+                      foregroundColor: Colors.white,
+                      padding: const EdgeInsets.all(20),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5)),
+                    ),
+                    onPressed: () {},
+                    child: const Text("Crédito"),
+                  )),
+              Container(
+                  padding: const EdgeInsets.only(
+                    left: 30,
+                    top: 15,
+                    right: 60,
+                    bottom: 0,
+                  ),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue, // background
+                      foregroundColor: Colors.white,
+                      padding: const EdgeInsets.all(20),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5)),
+                    ),
+                    onPressed: () {},
+                    child: const Text("Débito"),
+                  )),
+            ]),
+            Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+              Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(16),
+                  child: Text(
+                    'R\$ $_storedNumber',
+                    style: TextStyle(fontSize: 24),
+                  )),
+            ]),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ListView(
+                  physics: BouncingScrollPhysics(),
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        _buildNumberButton("1"),
+                        _buildNumberButton("2"),
+                        _buildNumberButton("3"),
+                        _buildNumberButton("-"),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        _buildNumberButton("4"),
+                        _buildNumberButton("5"),
+                        _buildNumberButton("6"),
+                        _buildNumberButton("_"),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        _buildNumberButton("7"),
+                        _buildNumberButton("8"),
+                        _buildNumberButton("9"),
+                        _buildNumberButton("x"),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        _buildNumberButton(","),
+                        _buildNumberButton("0"),
+                        _buildNumberButton("."),
+                        _buildNumberButton("ok"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ])),
+    );
   }
 }
